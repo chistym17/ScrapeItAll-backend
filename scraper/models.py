72496fde67985ca45f190a2c5f34765bf56d2934
chistym17 <mchisty31@gmail.com>
2025-02-02 14:43:26 +0600

    models updated

--- scraper/models.py
+++ scraper/models.py
@@ -6,6 +6,20 @@ class ScrapedData(models.Model):
     content = models.TextField()
     scraped_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50)
+    size = models.IntegerField(default=0)
+    selected = models.BooleanField(default=True)
+    processed = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.url} - {self.scraped_at}"
+
+
+class SitemapURL(models.Model):
+    url = models.URLField()
+    size = models.IntegerField(default=0)
+    selected = models.BooleanField(default=True)
+    processed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
